08559a801369cd33eeaf45ebbca9437bf6d49c9b
C K Ashby <clay.ashby@gmail.com>
2024-12-28 11:49:17 -0800

    Update posts view to render all-posts.html for correct page display

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
 def index(request):
     return render(request, 'blog/index.html')
 
+
 def posts(request):
-    # return render(request, 'blog/all-posts.html')
-    return HttpResponse("This is the blog posts page.")
+    return render(request, 'blog/all-posts.html')
+
 
 def post_details(request, slug):
     return HttpResponse(f"Hello, world. You're at the blog post {slug}.")
