1ca7d65ddccc9dddd7f14f5c7b32b56f704232d3
C K Ashby <clay.ashby@gmail.com>
2024-12-29 09:06:59 -0800

    Add sample blog post data for rendering in views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,25 @@
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
+
+posts = [
+    { 'slug': 'hike-in-the-mountains', 
+     'image': 'mountains.jpg', 
+     'author': 'JP Smith', 
+     'date': date.today(), # date(2021, 7, 21)
+     'title': 'Hike in the Mountains',
+      'excerpt': 'There\'s nothing like a day in the mountains.',
+      'content': """
+      Mountains are my favorite place to be. I go there every chance I get. The fresh air and the beautiful trees are a wonderful break from the city.
+      
+      There are many mountains to visit, but my favorite is the Black Mountain. It's the highest mountain in the state and the view from the top is amazing.
+      
+      When I hike in the mountains, I always take my camera with me. I love to take pictures of the beautiful scenery and the wildlife that lives there.
+      
+      If you've never been to the mountains, I highly recommend it. It's a great way to relax and get away from the hustle and bustle of everyday life.
+      """
+      },
+]
 
 
 def index(request):
